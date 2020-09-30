062bd13e5e9b5858e640dbf2e2dcf6fd85c8184a
Daniel <danielstevenrojas@hotmail.com>
2020-09-29 19:53:29 -0500

    organizando un poco el css

--- pages/index.js
+++ pages/index.js
@@ -20,47 +20,27 @@ export default function Home() {
       
         </h1>
 
-<nav>
-<ul>
-  <li>Dashboard</li>
-  <li>Población</li>
-  <li>Análisis</li>
-  <li>Filtros</li>
-  <li>Proyectos</li>
-  <li>Planes</li>
-</ul>
-<ul>
-  <li>Configuración</li>
-  <li>Ayuda</li>
-  <li>Salir</li>
-</ul>
+          <nav className={styles.nav}>
+          <ul>
+            <li>Dashboard</li>
+            <li>Población</li>
+            <li>Análisis</li>
+            <li>Filtros</li>
+            <li>Proyectos</li>
+            <li>Planes</li>
+          </ul>
+          <ul>
+            <li>Configuración</li>
+            <li>Ayuda</li>
+            <li>Salir</li>
+          </ul>
 
-</nav>
+          </nav>
 
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer>
+       
       </footer>
     </div>
   )
